7ae97ac6717fb45b5c2ec3041c0e7c3fe44cd014
William <williamkorte@hotmail.com>
2023-01-04 09:00:49 -0300

    analytics abilitado

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from "../components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
